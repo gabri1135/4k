b4ae43f23a3d1e953324289f2000db086aa71869
Gabriele <martinigabriele.gm@gmail.com>
2021-02-19 20:23:36 +0100

    prov2.0

--- get_m3u8.py
+++ get_m3u8.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver import Edge
 import requests
 import re
@@ -10,18 +11,17 @@ class M3U8:
     def get(self):
         root = Edge(
             executable_path="C:\\Users\\marti\\Downloads\\edgedriver_win64\\msedgedriver.exe")
+        root.get(self.url)
+        email=root.find_element_by_xpath("/html/body/div/div/div/div[2]/div[2]/form/div[1]/input")
+        email.send_keys("ggcarlo632@gmail.com")
+        pas=root.find_element_by_xpath("/html/body/div/div/div/div[2]/div[2]/form/div[2]/input")
+        pas.send_keys("sonoi00")
+        butt=root.find_element_by_xpath("/html/body/div/div/div/div[2]/div[2]/form/div[3]/button")
+        butt.submit()
+        page=re.findall(
+            'https://hdpass.click/movie/(.*)\?noads=1', root.page_source)[0]
+        root.get("https://hdpass.click/movie/%s?resolution=2&noads=1"%page)
 
-        headers = {"X-Secure-Proof": "pyTQsmrq-jBTIgtPhBK1kA"
-                   }
-        html = requests.get(self.url).text
-        iframe = re.findall(
-            '<iframe allowfullscreen src="(.*)"></iframe>', html)
-        m3u8 = re.findall(
-            'https://hdmario.live/embed/(.*)\?&amp;noads=1', iframe[0])
-        print(
-            'https://hdmario.live/embed/%s?loggedin=eyJlIjoiZ2djYXJsbzYzMkBnbWFpbC5jb20iLCJwIjoic29ub2RpMDAifQ==' % m3u8[0])
-        root.get('https://hdmario.live/embed/%s?loggedin=1613756278' %
-                 m3u8)
 
 # elem = driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/div/div[1]/form/div[1]/div[1]/div/label/input")
 # elem.send_keys("zeb98@gmail.co")
@@ -34,4 +34,4 @@ class M3U8:
 #     driver.get("https://www.netflix.com/logout")
 
 
-M3U8("https://hdpass.click/movie/28927?resolution=2&noads=1").get()
+M3U8("https://altadefinizionecommunity.net/flora-ulisse-streaming/?loggedin=eyJlIjoiZ2djYXJsbzYzMkBnbWFpbC5jb20iLCJwIjoic29ub2RpMDAifQ==").get()
