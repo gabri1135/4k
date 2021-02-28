d939d3ccc380acf60caa456399aeca6c02a19cd7
Gabriele <martinigabriele.gm@gmail.com>
2021-02-28 17:36:45 +0100

    solved : error

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def tempFolder(path, all=True):
     if path[-4:] == ".mp4":
         path = path.removesuffix(
             '.mp4')
-    tempName = path.split('\\')[-1].replace(' ', '_')
+    tempName = path.split('\\')[-1].replace(' ', '_').replace(':','-')
     if all:
         return '%s\\.%s' % (os.getcwd(), tempName)
     else:
@@ -22,7 +22,8 @@ def initialize(name, m3u8Path):
 
 def realPath(tempPath):
     tempPath=tempPath[1:]
-    return tempPath.replace('_', ' ')
+    return tempPath.replace('_', ' ').replace('-',':')
+
 
 def dir(path):
     temp=path.split('\\')[-1]
